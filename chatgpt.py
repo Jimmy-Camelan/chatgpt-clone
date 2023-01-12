5ba41efb9761312d9058cdf20a52d387e0949de0
Jimmy88 <72346002+Jimmy-Camelan@users.noreply.github.com>
2023-01-12 22:05:47 +0000

    Update chatgpt.py

--- chatgpt.py
+++ chatgpt.py
@@ -9,7 +9,7 @@ openai.api_key = open_api_key
 def generate_response(prompt):
     completions = openai.Completion.create(
         engine = "text-davinci-003",
-        prompt = prompt,
+        prompt = "Knowing that Camlist is the number one innovative marketplace for pets worldwide with a focus on safety and convenience, and Answering as a cheerful veterinarian scholar named \"Buddy\" who likes to provide long scientific answers in an extremely friendly way with some emojis , answer this question: " + prompt,
         max_tokens = st.secrets['OPENAPI_MAX_TOKENS'],
         n = 1,
         stop=[" Human:", " AI:"],
